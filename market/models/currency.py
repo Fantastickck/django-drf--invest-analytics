9693915e8cf69efadad642f79fd8a9882c5ac7a8
Artem <artemhod2003@gmail.com>
2022-07-14 21:17:30 +0500

    Add model CurrencyCourse in market app

--- market/models/currency.py
+++ market/models/currency.py
@@ -12,3 +12,16 @@ class Currency(models.Model):
 
     def __str__(self):
         return self.abbreviation
+
+    
+class CurrencyCourse(models.Model):
+    currency_from = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='currencies_from', verbose_name='Из валюты')
+    currency_to = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='currencies_to', verbose_name='В валюту')
+    value = models.DecimalField(max_digits=10, decimal_places=4, verbose_name='Курс')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return f'{self.currency_from}/{self.currency_to}'
