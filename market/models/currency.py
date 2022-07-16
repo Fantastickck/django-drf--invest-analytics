b560daff964506e1ebf98fcfc638468458c946ed
Artem <artemhod2003@gmail.com>
2022-07-16 22:54:27 +0500

    Change position, currency models, add CurrencyPosition

--- market/models/currency.py
+++ market/models/currency.py
@@ -1,8 +1,15 @@
 from django.db import models
 
 
+CURRENCY_TYPE = (
+    ('Валюта', 'CURRENCY'),
+    ('Криптовалюта', 'CRYPTOCURRENCY')
+)
+
+
 class Currency(models.Model):
     name = models.CharField(max_length=255, verbose_name='Наименование')
+    type_currency = models.CharField(max_length=20, choices=CURRENCY_TYPE, verbose_name='Тип валюты')
     abbreviation = models.CharField(max_length=10, verbose_name='Аббревиатура')
     symbol = models.CharField(max_length=10, verbose_name='Символ')
 
@@ -15,9 +22,17 @@ class Currency(models.Model):
 
     
 class CurrencyCourse(models.Model):
-    currency_from = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='currencies_from', verbose_name='Из валюты')
-    currency_to = models.ForeignKey(Currency, on_delete=models.CASCADE, related_name='currencies_to', verbose_name='В валюту')
-    value = models.DecimalField(max_digits=10, decimal_places=4, verbose_name='Курс')
+    currency_from = models.ForeignKey(
+        Currency, 
+        on_delete=models.CASCADE, 
+        related_name='currencies_from', 
+        verbose_name='Из валюты')
+    currency_to = models.ForeignKey(
+        Currency, on_delete=models.CASCADE, 
+        related_name='currencies_to', 
+        verbose_name='В валюту')
+    value = models.DecimalField(
+        max_digits=20, decimal_places=6, verbose_name='Курс')
 
     class Meta:
         verbose_name = 'Курс'
